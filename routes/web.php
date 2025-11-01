f5fcda80d62dfd445acb7395b00ccd0899c496e5
Edgar-Poot <edgar.dev25@gmail.com>
2025-10-31 22:11:09 -0600

    Study: Agregar ejemplo de Query Builder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
@@ -96,4 +97,70 @@ Route::get('categoria', function () {
 });
 Route::get('contacto', function () {
     return view('contacto');
+});
+
+//Query builder
+//Obtener datos
+Route::get('query', function () {
+    $entradas = DB::table('entradas')->get();
+    return $entradas;
+});
+
+//Obtener el primer registro
+Route::get('find', function () {
+    $entradas = DB::table('entradas')->first();
+    return $entradas;
+});
+
+//Filtrado de datos
+Route::get('filtro', function () {
+    $entradas = DB::table('entradas')
+        ->where('user_id', 1)
+        ->where('titulo', 'LIKE', 'a%') //comienzan con la letra A
+        ->orWhere('titulo', 'LIKE', 'b%')->get();
+    return $entradas;
+});
+
+//Otros métodos que nos van a permitir realizar consultas tambien.
+//whereNull()
+//whereNotNull()
+//whereIn()
+//whereNotIn()
+//whereBetween
+//whereNotBetween
+
+//JOINS con query builder
+
+Route::get('join', function () {
+    $entradas = DB::table('entradas')
+        ->join('users', 'entradas.user_id', '=', 'users.id')
+        //->select('entradas.id', 'entradas.titulo', 'entradas.tag', 'entradas.imagen', 'users.email')
+        ->get();
+    return $entradas;
+});
+
+//leftJoin()
+//rightJoin()
+//joinWhere()
+
+// Inserción de registros
+Route::get('/insert', function () {
+    $insertado = DB::table('users')
+        ->insert([
+            "name" => "Juan Pérez",
+            "email" => "juan@prueba.com",
+            "password" => "juan"
+        ]);
+    return $insertado;
+});
+
+//Obtener
+Route::get('/getId', function () {
+    $id = DB::table('users')
+        ->insertGetId([
+            "name" => "Juan Pérez",
+            "email" => "juan2@prueba.com",
+            "password" => "juan2"
+        ]);
+    return $id;
 });
