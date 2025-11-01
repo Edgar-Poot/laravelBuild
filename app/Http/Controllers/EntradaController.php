c578f185efdffa86d305cf63167f6473813af5aa
Edgar-Poot <edgar.dev25@gmail.com>
2025-11-01 15:04:48 -0600

    Study: Validar datos utilizando FormRequest

--- app/Http/Controllers/EntradaController.php
+++ app/Http/Controllers/EntradaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Entrada;
 use Illuminate\Http\Request;
 use PhpParser\Node\Stmt\Return_;
+use App\Http\Requests\EntradaRequest;
 
 class EntradaController extends Controller
 {
@@ -34,30 +35,8 @@ class EntradaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(EntradaRequest $request)
     {
-        /**
-         * Antes de crear el modelo y guardarlo hay que validar
-         * Aunque validar en el controlador funciona. Es una mala practica
-         * Porque sobrecarga el controlador con lógica que debería manejarse por separado
-         */
-        $request->validate([
-            'tag' => 'required|string|max:20',
-            'titulo' => 'required|string|max:50',
-            'contenido' => 'required|string'
-        ], [
-            'titulo.required' => 'El campo de título es obligatorio.',
-            'titulo.string' => 'El título debe ser una cadena de texto.',
-            'titulo.max' => 'El título no puede superar los 50 caracteres.',
-
-            'tag.required' => 'El campo de tag es obligatorio.',
-            'tag.string' => 'El tag debe ser una cadena de texto.',
-            'tag.max' => 'El tag no puede superar los 20 caracteres.',
-
-            'contenido.required' => 'El campo de contenido es obligatorio.',
-            'contenido.string' => 'El contenido debe ser una cadena de texto.',
-        ]);
-
         $entrada = new Entrada();
         $entrada->titulo = $request->input('titulo');
         $entrada->tag = $request->input('tag');
---
+++ app/Http/Requests/EntradaRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EntradaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; //Cambiar false por true sino la solicitud será rechazada
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'titulo' => 'required|string|min:5|max:50',
+            'tag' => 'required|string|min:3|max:20',
+            'contenido' => 'required|string|min:5|max:255'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'titulo.required' => 'El campo de título es obligatorio.',
+            'titulo.string' => 'El título debe ser una cadena de texto.',
+            'titulo.max' => 'El título no puede superar los 50 caracteres.',
+
+            'tag.required' => 'El campo de tag es obligatorio.',
+            'tag.string' => 'El tag debe ser una cadena de texto.',
+            'tag.max' => 'El tag no puede superar los 20 caracteres.',
+
+            'contenido.required' => 'El campo de contenido es obligatorio.',
+            'contenido.string' => 'El contenido debe ser una cadena de texto.',
+        ];
+    }
+}
